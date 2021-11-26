38ceab73bf425a50e3f620f6d6925e942f7dacd8
Michal Strzezon <mstrzezon@gmail.com>
2021-11-26 18:05:31 +0100

    add_docs

--- poker-model/src/main/java/pl/edu/agh/kis/pz1/player/Player.java
+++ poker-model/src/main/java/pl/edu/agh/kis/pz1/player/Player.java
@@ -1,11 +1,39 @@
 package pl.edu.agh.kis.pz1.player;
 
+/**
+ * Player is the class, which represents poker player.
+ * A player object encapsulates all the most important information of player like:
+ *  <ul>
+ *      <li>id</li>
+ *      <li>isInPlay</li>
+ *      <li>funds</li>
+ *      <li>hand</li>
+ *  </ul>
+ *
+ * @author Michał Strzeżoń
+ */
 public class Player {
+    /**
+     * player id.
+     */
     private final int id;
+    /**
+     * information if player is still player.
+     */
     private boolean isInPlay;
+    /**
+     * player funds.
+     */
     private int funds;
-    private Hand hand;
+    /**
+     * player hand with cards.
+     */
+    private final Hand hand;
 
+    /**
+     * Creates player with id and 50 funds.
+     * @param id player id.
+     */
     public Player(int id) {
         this.id = id;
         funds=50;
@@ -13,19 +41,46 @@ public class Player {
         hand = new Hand();
     }
 
+    /**
+     * Gets player id.
+     * @return player id.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets player money.
+     * @return player funds.
+     */
     public int getFunds() { return funds; }
 
+    /**
+     * Gets isInPlay
+     * @return   <code>true</code> if player is in the game.
+     *           <code>false</code> otherwise.
+     */
     public boolean getIsInPlay() { return isInPlay;}
 
+    /**
+     * Sets isInPlay
+     * @param state <code>true</code> if player is in the game.
+     *              <code>false</code> otherwise.
+     */
     public void setIsInPlay(boolean state) { isInPlay = state; }
 
+    /**
+     * Gets player hand.
+     * @return player hand.
+     */
     public Hand getHand() { return hand; }
 
-    /** it will be done */
+    /**
+     * Calls in the game.
+     * @param wage wage that the player wants to bet.
+     * @return     <code>true</code> if call succeeds.
+     *             <code>false</code> otherwise.
+     */
     public boolean call(int wage) {
         if (funds - wage > 0) {
             funds -= wage;
@@ -34,7 +89,12 @@ public class Player {
         else return false;
     }
 
-    /** it will be done */
+    /**
+     * Raises in the game.
+     * @param newWage wage that the player wants to bet.
+     * @return        <code>true</code> if call succeeds.
+     *                <code>false</code> otherwise.
+     */
     public boolean raise(int newWage) {
         if (funds - newWage > 0) {
             funds -= newWage;
@@ -43,6 +103,9 @@ public class Player {
         else return false;
     }
 
+    /**
+     * Folds in the game.
+     */
     public void fold() {
         isInPlay = false;
     }
