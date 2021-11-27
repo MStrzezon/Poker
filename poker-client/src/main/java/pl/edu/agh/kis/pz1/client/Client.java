46b277907e9ea45e6e01c2af269c2a9cd5cba1e7
Michal Strzezon <mstrzezon@gmail.com>
2021-11-27 18:00:55 +0100

    logge_problem

--- poker-client/src/main/java/pl/edu/agh/kis/pz1/client/Client.java
+++ poker-client/src/main/java/pl/edu/agh/kis/pz1/client/Client.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Client {
     private Socket socket;
