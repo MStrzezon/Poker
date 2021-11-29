d8ef65fc36a7a040011369f0d20622c11cc24ac7
Michal Strzezon <mstrzezon@gmail.com>
2021-11-29 20:18:18 +0100

    add_docs

--- poker-server/src/main/java/pl/edu/agh/kis/pz1/protocol/GameProtocol.java
+++ poker-server/src/main/java/pl/edu/agh/kis/pz1/protocol/GameProtocol.java
@@ -121,7 +121,7 @@ public class GameProtocol {
                 return raise(userId, parameters);
             }
             case "/fold" -> {
-                return fold(userId, parameters);
+                return fold(userId);
             }
             case "/draw" -> {
                 return draw(userId, parameters);
@@ -168,10 +168,10 @@ public class GameProtocol {
     }
 
     /**
-     * Informs about current state.
+     * Support the case /state.
      * @return  <code>Game not created!</code>.
      *          <code>Game created but not started!</code>
-     *          <code>It's: ``current round`` round. Now playing player nr: ``current player```</code>
+     *          <code>It's: `current round` round. Now playing player nr: `current player`</code>
      *          <code>It's a draw time!</code>
      *          <code>Game is finished. Enter /result to show result.</code>
      */
@@ -193,17 +193,19 @@ public class GameProtocol {
         }
         return new String[2];
     }
-//TODO create docs below
+
     /**
-     * Creates a game.
+     * Supports the case /create.
      * @param parameters ante
-     * @return information about execution of the creating a game.
+     * @return           <code>Game was created! Ante: `ante`</code>.
+     *                   <code>You should enter ante! Max ante: 10</code>
+     *                   <code>The game was already created! Enter /join to join to the game!</code>
      */
     private String[] create(List<Integer> parameters) {
         if (state == GameState.NOT_CREATED) {
             if (parameters.size() == 1 && parameters.get(0) > 0 && parameters.get(0) <= 10) {
                 game = new Game(1, parameters.get(0));
-                return stateToEnd(GameState.CREATED, "Game was created! Ante: " + parameters.get(0));
+                return changeState(GameState.CREATED, "Game was created! Ante: " + parameters.get(0));
             } else {
                 return new String[]{"ONE", "You should enter ante! Max ante: 10"};
             }
@@ -213,9 +215,12 @@ public class GameProtocol {
     }
 
     /**
-     * Joins to the game
+     * Supports the case /join.
      * @param userId id of user.
-     * @return       information about execution of the joining to the game.
+     * @return       <code>Too many participants. Max number of players: `max_number_of_players`</code>.
+     *               <code>You joined the game, your number in game: `player_game_id`</code>
+     *               <code>You are already join to the game</code>
+     *               <code>Game already started. You cannot join</code>
      */
     private String[] join(int userId) {
         if (state == GameState.NOT_CREATED) return new String[]{"ONE",
@@ -234,7 +239,15 @@ public class GameProtocol {
         return new String[2];
     }
 
-
+    /**
+     * Supports the case /start.
+     * @param userId id of user.
+     * @return        <code>The game has not been created yet! Enter /create {ante} to create a game! Max ante: 10</code>
+     *                <code>Wait for another player! Minimum number of players: 2</code>.
+     *                <code>Game was started! Player number 0 play! All funds: `funds` + Current bet: `bet`. Your funds: `player money`</code>
+     *                <code>You should first join to the game!</code>
+     *                <code>Game already started!</code>
+     */
     private String[] start(int userId) {
         if (state == GameState.NOT_CREATED) return new String[]{"ONE",
                 ENTER_TO_CREATE_GAME};
@@ -257,6 +270,11 @@ public class GameProtocol {
         return new String[2];
     }
 
+    /**
+     * Supports the case /players.
+     * @return  <code>The game has not been created yet! Enter /create {ante} to create a game! Max ante: 10</code>
+     *          <code>`number of players`</code>
+     */
     private String[] players() {
         if (state == GameState.NOT_CREATED) {
             return new String[]{"ONE", "\n" +
@@ -266,6 +284,13 @@ public class GameProtocol {
         }
     }
 
+    /**
+     * Supports the case /id.
+     * @param userId id of user.
+     * @return       <code>The game has not been created yet! Enter /create {ante} to create a game! Max ante: 10</code>
+     *               <code>You should join to the game to see your id in game!</code>
+     *               <code>Your game id: `player id`.</code>
+     */
     private String[] id(int userId) {
         if (state == GameState.NOT_CREATED) {
             return new String[]{"ONE", ENTER_TO_CREATE_GAME};
@@ -276,6 +301,13 @@ public class GameProtocol {
         }
     }
 
+    /**
+     * Supports the case /money.
+     * @param userId id of user.
+     * @return       <code>The game has not been created yet! Enter /create {ante} to create a game! Max ante: 10</code>
+     *               <code>You should join to the game to see your money in game!</code>
+     *               <code>Money: `player money`</code>
+     */
     private String[] money(int userId) {
         if (state == GameState.NOT_CREATED) {
             return new String[]{"ONE", ENTER_TO_CREATE_GAME};
@@ -286,6 +318,13 @@ public class GameProtocol {
         }
     }
 
+    /**
+     * Supports the case /hand.
+     * @param userId id of user.
+     * @return       <code>You must play the game to display your deck of cards!</code>
+     *               <code>`cards`</code>
+     *               <code>You are not playing the game. Wait for the end of game!</code>
+     */
     private String[] hand(int userId) {
         if (state == GameState.NOT_CREATED || state == GameState.CREATED)
             return new String[]{"ONE", "You must play the game to display your deck of cards!"};
@@ -305,6 +344,14 @@ public class GameProtocol {
         return new String[2];
     }
 
+    /**
+     * Supports the case /call
+     * @param userId id of user.
+     * @return <code>You must play the game to be able to call!</code>
+     *         <code>round_call(userId)</code>
+     *         <code>Now is time to draw. You cannot call!</code>
+     *         <code>Game is over. Enter /result to see result.</code>
+     */
     private String[] call(int userId) {
         if (state == GameState.NOT_CREATED || state == GameState.CREATED) return new String[]{"ONE", PLAY_GAME_TO_CALL};
         if (state == GameState.ROUND) return round_call(userId);
@@ -313,6 +360,13 @@ public class GameProtocol {
         return new String[2];
     }
 
+    /**
+     * Calls
+     * @param userId id of user.
+     * @return       <code>updateStateCall(previousPlayer, previousRound)</code>
+     *               <code>You don't have money!</code>
+     *               <code>Not your turn!</code>
+     */
     private String[] round_call(int userId) {
         int playerIndex = game.getPlayers().indexOf(game.getPlayer(userId));
         if (game.getCurrentPlayer() == playerIndex) {
@@ -326,23 +380,49 @@ public class GameProtocol {
         }
     }
 
+    /**
+     * Changes state after call/
+     * @param previousPlayer previous player
+     * @param previousRound  previous round
+     * @return               <code>stateToDraw(previousPlayer, " called. Now time to draw!\nAll funds: ")</code>
+     *                       <code>stateToEnd(GameState.END, SEE_RESULT)</code>
+     *                       <code>PLAYER `previousPlayer` called. Now player `current player` ALL_FUNDS 'funds'
+     *                             CURRENT_BET `bet`</code>
+     */
     private String[] updateStateCall(int previousPlayer, int previousRound) {
         if (game.getCurrentRound() == previousRound +1 && previousRound ==1) return stateToDraw(previousPlayer, " called. Now time to draw!\nAll funds: ");
         else if (game.getCurrentRound() == previousRound +1 && previousRound ==2) {
-            return stateToEnd(GameState.END, SEE_RESULT);
+            return changeState(GameState.END, SEE_RESULT);
         }
         return new String[]{"MORE", PLAYER + previousPlayer + " called. Now player " + game.getCurrentPlayer() + ALL_FUNDS + game.getAllFunds()
                 + CURRENT_BET + game.getBet()};
     }
 
+    /**
+     * Supports the case /raise
+     * @param userId     id of user
+     * @param parameters new bet
+     * @return           <code>You must play the game to be able to raise!</code>
+     *                   <code>round_raise(userId, parameters)</code>
+     *                   <code>Now is time to draw. You cannot raise!</code>
+     *                   <code>Game is over. Enter /result to see result.</code>
+     */
     private String[] raise(int userId, List<Integer> parameters) {
-        if (state==GameState.NOT_CREATED || state == GameState.CREATED) return new String[]{"ONE", PLAY_GAME_TO_CALL};
+        if (state==GameState.NOT_CREATED || state == GameState.CREATED) return new String[]{"ONE", "You must play the game to be able to raise!"};
         if (state==GameState.ROUND) return round_raise(userId, parameters);
         if (state == GameState.DRAW) return new String[]{"ONE", "Now is time to draw. You cannot raise!"};
         if (state == GameState.END) return new String[]{"ONE", SEE_RESULT};
         return new String[2];
     }
-
+    /**
+     * Raises
+     * @param userId id of user.
+     * @param parameters new bet
+     * @return           <code>Enter new wage!</code>
+     *                   <code>updateStateRaise(previousPlayer, previousRound)</code>
+     *                   <code>You should raise bet or you don't have money!</code>
+     *                   <code>Not your turn!</code>
+     */
     private String[] round_raise(int userId, List<Integer> parameters) {
         int playerIndex = game.getPlayers().indexOf(game.getPlayer(userId));
         if (game.getCurrentPlayer() == playerIndex) {
@@ -357,31 +437,52 @@ public class GameProtocol {
         }
     }
 
+    /**
+     * Changes state after raise
+     * @param previousPlayer previous player
+     * @param previousRound  previous round
+     * @return               <code>stateToDraw(previousPlayer, " called. Now time to draw!\nAll funds: ")</code>
+     *                       <code>stateToEnd(GameState.END, SEE_RESULT)</code>
+     *                       <code>PLAYER `previousPlayer` raised. Now player `current player` ALL_FUNDS 'funds'
+     *                             CURRENT_BET `bet`</code>
+     */
     private String[] updateStateRaise(int previousPlayer, int previousRound) {
         if (game.getCurrentRound() == previousRound+1) {
             return stateToDraw(previousPlayer, " raised. Now time to draw!\nAll funds: ");
         } else if (game.getCurrentRound() == previousRound+1 && previousRound==2) {
-            return stateToEnd(GameState.END, SEE_RESULT);
+            return changeState(GameState.END, SEE_RESULT);
         }
         return new String[]{"MORE", PLAYER + previousPlayer + " raised. Now player "+ game.getCurrentPlayer() + ALL_FUNDS
                 + game.getAllFunds()
                 + CURRENT_BET + game.getBet()};
     }
 
-    private String[] fold(int userId, List<Integer> parameters) {
+    /**
+     * Supports the case /fold
+     * @param userId id of user
+     * @return       <code>You must play the game to be able to fold!</code>
+     *               <code>stateToDraw(previousPlayer, " folded. Now time to draw!\nAll funds: ")</code>
+     *               <code>stateToEnd(GameState.END, SEE_RESULT)</code>
+     *               <code>PLAYER `previousPlayer` folded. Now player `current player` ALL_FUNDS 'funds'
+     *                     CURRENT_BET `bet`</code>
+     *               <code>Not your turn!</code>
+     *               <code>Now is time to draw. You cannot fold!</code>
+     *               <code>Game is over. You cannot fold!</code>
+     */
+    private String[] fold(int userId) {
         if (state == GameState.NOT_CREATED || state == GameState.CREATED)
-            return new String[]{"ONE", PLAY_GAME_TO_CALL};
+            return new String[]{"ONE", "You must play the game to be able to fold!"};
         if (state == GameState.ROUND) {
             int playerIndex = game.getPlayers().indexOf(game.getPlayer(userId));
             if (game.getCurrentPlayer() == playerIndex) {
                 int previousPlayer = game.getCurrentPlayer();
                 int previousRound = game.getCurrentRound();
-                game.makeAMove(3, parameters.get(0), game.getPlayers().get(playerIndex));
+                game.makeAMove(3, 0, game.getPlayers().get(playerIndex));
                 if (game.getCurrentRound() == previousRound+1) {
                     return stateToDraw(previousPlayer, " folded. Now time to draw!\nAll funds: ");
                 }
                 else if (game.getCurrentRound() == previousRound+1 && previousRound==2) {
-                    return stateToEnd(GameState.END, SEE_RESULT);
+                    return changeState(GameState.END, SEE_RESULT);
                 }
                 return new String[]{"MORE", PLAYER + previousPlayer + " folded. Now player " +
                         game.getCurrentPlayer() + ALL_FUNDS + game.getAllFunds()
@@ -395,6 +496,14 @@ public class GameProtocol {
         return new String[2];
     }
 
+    /**
+     * Changes state to draw with additional information.
+     * @param previousPlayer previous player
+     * @param s              string to print
+     * @return               <code>PLAYER + previousPlayer + s
+     *                             + game.getAllFunds()
+     *                             + CURRENT_BET + game.getBet()</code>
+     */
     private String[] stateToDraw(int previousPlayer, String s) {
         state = GameState.DRAW;
         return new String[]{"MORE", PLAYER + previousPlayer + s
@@ -402,6 +511,15 @@ public class GameProtocol {
                 + CURRENT_BET + game.getBet()};
     }
 
+    /**
+     * Suports the case /draw
+     * @param userId     id of user
+     * @param parameters cards to draw
+     * @return           <code>You must play the game to be able to draw!</code>
+     *                   <code>This is not the time to draw!</code>
+     *                   <code>drawDraw(userId, parameters)</code>
+     *                   <code>Game is over. You cannot draw!</code>
+     */
     private String[] draw(int userId, List<Integer> parameters) {
         if (state == GameState.NOT_CREATED || state==GameState.CREATED) return new String[]{"ONE", "You must play the game to be able to draw!"};
         if (state==GameState.ROUND) return new String[]{"ONE", "This is not the time to draw!"};
@@ -412,12 +530,28 @@ public class GameProtocol {
         return new String[2];
     }
 
+    /**
+     * Draws with additional information
+     * @param userId     id of user
+     * @param parameters cards to draw
+     * @return           <code>currentPlayerDraw(userId, parameters)</code>
+     *                   <code>Not your turn!</code>
+     */
     private String[] drawDraw(int userId, List<Integer> parameters) {
         int playerIndex = game.getPlayers().indexOf(game.getPlayer(userId));
         if (game.getCurrentDrawPlayer() == playerIndex) return currentPlayerDraw(userId, parameters);
         else return new String[]{"MORE", NOT_YOUR_TURN};
     }
 
+    /**
+     * Support drawDraw function
+     * @param userId     id of user
+     * @param parameters cards to draw
+     * @return           <code>msgAfterDraw()</code>    
+     *                   <code>Number of cards must be unique!</code>
+     *                   <code>You should enter cards number from 0 to 4!</code>
+     *                   <code>You should enter max 5 cards!</code>
+     */
     private String[] currentPlayerDraw(int userId, List<Integer> parameters) {
         if (parameters.size()==0) {
             return msgAfterDraw();
@@ -436,13 +570,24 @@ public class GameProtocol {
         } else return new String[]{"MORE", "You should enter max 5 cards!"};
     }
 
+    /**
+     * Create message after draw
+     * @return <code>End of draw. Now player `current player`</code>
+     *         <code>Now player `current player`</code>
+     */
     private String[] msgAfterDraw() {
         if (!game.nextDrawPlayer()) {
-            return stateToEnd(GameState.ROUND, "End of draw. Now player " + game.getCurrentPlayer());
+            return changeState(GameState.ROUND, "End of draw. Now player " + game.getCurrentPlayer());
         }
         return new String[]{"MORE", "Now player " + game.getCurrentDrawPlayer()};
     }
 
+    /**
+     * Supports the case /result
+     * @return <code>You should play the game to see result!</code>
+     *         <code>Game is not over yet</code>
+     *         <code>`result`</code>
+     */
     private String[] result() {
         if (state==GameState.NOT_CREATED || state==GameState.CREATED) return new String[]{"ONE", "You should play the game to see result!"};
         if (state==GameState.ROUND || state==GameState.DRAW) return new String[]{"ONE", "Game is not over yet"};
@@ -458,20 +603,36 @@ public class GameProtocol {
         return new String[2];
     }
 
+    /**
+     * Supports the case /end
+     * @return <code>You can end game after all rounds!</code>
+     *         <code>Game is over. To create new enter /create.</code>
+     */
     private String[] end() {
         if (state==GameState.NOT_CREATED || state==GameState.CREATED || state==GameState.ROUND || state==GameState.DRAW) return new String[]{"ONE", "You can end game after all rounds!"};
         if (state==GameState.END) {
             game = null;
-            return stateToEnd(GameState.NOT_CREATED, "Game is over. To create new enter /create.");
+            return changeState(GameState.NOT_CREATED, "Game is over. To create new enter /create.");
         }
         return new String[2];
     }
 
-    private String[] stateToEnd(GameState end, String seeResult) {
-        state = end;
-        return new String[]{"MORE", seeResult};
+    /**
+     * Changes state
+     * @param newState    new state
+     * @param information additional information
+     * @return            <code>information</code>
+     */
+    private String[] changeState(GameState newState, String information) {
+        state = newState;
+        return new String[]{"MORE", information};
     }
 
+    /**
+     * Support the case ""
+     * @param userId id of user
+     * @return information about current situation
+     */
     private String[] emptyMessage(int userId) {
         String message = "";
         if (state==GameState.NOT_CREATED) message = "Game not created yet";
